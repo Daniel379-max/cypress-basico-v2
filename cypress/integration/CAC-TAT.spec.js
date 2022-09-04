6bbf8e77ba02c710986047caf458178fc8d73c30
Daniel Romagnoli <danielromagnoli12@gmail.com>
2022-09-04 09:32:06 -0300

    modificate archive CAC-TAT

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -60,9 +60,9 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.error').should('be.visible')
     })
 
-    it('envia o formuário com sucesso usando um comando customizado', () => {
+    it.only('envia o formuário com sucesso usando um comando customizado', () => {
         cy.fillMandatoryFieldsAndSubmit()
-        cy.get('.error').should('be.visible')
+        cy.get('.success').should('be.visible')
     })
 
     it('seleciona um produto por seu texto', () => {
